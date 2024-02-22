355b8344c488a413e6bcac0d42803bbf66a3be54
MAlejandroR <manuelromeromiguel@gmail.com>
2024-02-22 09:32:20 +0100

    con los vebos get implementados

---
+++ app/Http/Controllers/AlumnoController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\AlumnoCollection;
+use App\Http\Resources\AlumnoResource;
+use App\Models\Alumno;
+use Illuminate\Http\Request;
+
+class AlumnoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $alumnos= Alumno::all();
+//        return response()->json($alumnos);
+        return new AlumnoCollection($alumnos);
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Alumno $alumno)
+    {
+        return new AlumnoResource($alumno);
+//        return response()->json($alumno);
+
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Alumno $alumno)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Alumno $alumno)
+    {
+        //
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Alumno;
+use Database\Factories\AlumnoFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Alumno::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
